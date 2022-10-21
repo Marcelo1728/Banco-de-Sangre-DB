0d41562812a8af21268f30a66c07cfe3d980b6df
GustavoMnz <59345599+GustavoMnz@users.noreply.github.com>
2022-10-20 22:34:39 -0300

    Crear tablas tipo_sangre y tabla sexo.

--- Banco_de_Sangre.sql
+++ Banco_de_Sangre.sql
@@ -2,3 +2,19 @@ CREATE DATABASE Banco_de_Sangre;
 GO
 USE Banco_de_Sangre;
 GO
+
+CREATE TABLE tipo_sangre
+(
+	tipo_sangre_id INT IDENTITY PRIMARY KEY,
+	grupo_sanguineo CHAR(2) not null,
+	factor_rh CHAR(1) not null,
+	CONSTRAINT CK_tipo_sangre_grupo_sanguineo CHECK (grupo_sanguineo in ('A','B','AB','O')),
+	CONSTRAINT CK_tipo_sangre_factor_rh CHECK (factor_rh in ('+','-')),
+);
+
+CREATE TABLE sexo
+(
+	id_sexo INT IDENTITY PRIMARY KEY,
+	descripcion CHAR(1) not null,
+	CONSTRAINT CK__sexo__descripcion CHECK (descripcion in ('F','M')),
+);
