5e6bca80f20add8ff0bf0541edcd3cb41e08fecc
milagrosna13 <116464714+milagrosna13@users.noreply.github.com>
2022-10-22 21:20:14 -0300

    Update Banco_de_Sangre.sql

--- Banco_de_Sangre.sql
+++ Banco_de_Sangre.sql
@@ -40,3 +40,32 @@ create table donante
 	constraint FK__donante__sexo foreign key (id_sexo) references sexo (id_sexo),
 	constraint FK__donante__tipo_sangre foreign key (tipo_sangre_id) references tipo_sangre (tipo_sangre_id)
 );
+
+create table paciente 
+(
+	dni char(8) unique not null,
+	nombre varchar(20) not null,
+	apellido varchar(20) not null,
+	id_sexo int not null,
+	fecha_nacimiento date not null,
+	telefono varchar(20),
+	direccion varchar(40),
+	tipo_sangre_id int not null,
+	constraint UQ__paciente_dni unique (dni),
+	constraint FK__paciente__sexo foreign key (id_sexo) references sexo (id_sexo),
+	constraint FK__paciente__tipo_sangre foreign key (tipo_sangre_id) references tipo_sangre (tipo_sangre_id)
+);
+
+create table bioanalista 
+(
+	id_bioanalista int identity primary key,
+	dni char(8) unique not null,
+	nombre varchar(20) not null,
+	apellido varchar(20) not null,
+	id_sexo int not null,
+	fecha_nacimiento date not null,
+	telefono varchar(20),
+	direccion varchar(40),
+	constraint UQ__bioanalista_dni unique (dni),
+	constraint FK__bioanalista__sexo foreign key (id_sexo) references sexo (id_sexo),
+);
